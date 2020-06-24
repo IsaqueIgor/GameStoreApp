6d36dbb7f18b1428c4a6cf833075cf83e998b277
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-06-24 13:25:52 -0300

    Feat: Live Screeen

--- src/Screens/Live/index.js
+++ src/Screens/Live/index.js
@@ -1,6 +1,96 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StatusBar } from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+import Text from '../../components/Text';
+import {
+  Container,
+  Header,
+  SearchContainer,
+  Search,
+  LiveGame,
+  LiveGameBadge,
+  LiveGameContainer,
+  LiveGameOver,
+  LiveGameTitle,
+  LiveGames,
+  LiveGamesTitle,
+  PopularGame,
+  PopularGameContainer,
+  PopularGames,
+  SearchIcon,
+  SectionContainer,
+} from './styles';
 
 export default Live = () => {
-  return <Text>Live</Text>;
+  return (
+    <Container>
+      <StatusBar translucent backgroundColor='transparent' />
+
+      <Header>
+        <Text large bold>
+          Streaming
+        </Text>
+        <SearchContainer>
+          <Search
+            placeholder='Search live streams or games...'
+            placeholderTextColor='#838383'
+          />
+          <SearchIcon>
+            <Entypo name='magnifying-glass' size={24} color='#838383' />
+          </SearchIcon>
+        </SearchContainer>
+      </Header>
+
+      <SectionContainer>
+        <Text medium bold>
+          Popular Games
+        </Text>
+
+        <PopularGames horizontal={true} showsHorizontalScrollIndicator={false}>
+          {games.map((game, index) => {
+            return (
+              <PopularGameContainer key={index}>
+                <PopularGame source={game.cover} />
+              </PopularGameContainer>
+            );
+          })}
+        </PopularGames>
+      </SectionContainer>
+
+      <SectionContainer>
+        <LiveGamesTitle>
+          <Text medium bold>
+            Live Now
+          </Text>
+          <Text bold color='#819ee5'>
+            View all
+          </Text>
+        </LiveGamesTitle>
+      </SectionContainer>
+
+      <LiveGames
+        data={games}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <LiveGameContainer>
+            <LiveGame source={item.cover} />
+            <LiveGameOver>
+              <LiveGameTitle>
+                <Text small bold>
+                  {item.title}
+                </Text>
+              </LiveGameTitle>
+              <LiveGameBadge>
+                <Text small bold>
+                  Live
+                </Text>
+                <Entypo name='rss' size={8} color='#FFF' />
+              </LiveGameBadge>
+            </LiveGameOver>
+          </LiveGameContainer>
+        )}
+      />
+    </Container>
+  );
 };
---
+++ src/Screens/Live/styles.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Container = styled.SafeAreaView`
+  background-color: #040617;
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  margin: 32px 16px 0 16px;
+`;
+
+export const SearchContainer = styled.View`
+  position: relative;
+  margin: 32px 8px;
+  background-color: #404040;
+  border-radius: 100px;
+  justify-content: center;
+`;
+
+export const Search = styled.TextInput`
+  padding: 16px 44px 16px 32px;
+  color: #c6c6c6;
+`;
+
+export const SearchIcon = styled.TouchableOpacity`
+  position: absolute;
+  right: 16px;
+`;
+
+export const SectionContainer = styled.View`
+  margin: 0 16px;
+`;
+
+export const PopularGames = styled.ScrollView`
+  margin: 16px 0 12px -12px;
+`;
+
+export const PopularGameContainer = styled.View`
+  width: 80px;
+  margin: 0 12px;
+`;
+
+export const PopularGame = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 12px;
+`;
+
+export const LiveGamesTitle = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LiveGames = styled.FlatList`
+  flex: 1;
+  margin: 16px 16px 0 16px;
+`;
+
+export const LiveGameContainer = styled.View`
+  position: relative;
+`;
+
+export const LiveGame = styled.Image`
+  width: 100%;
+  height: 275px;
+  margin-bottom: 16px;
+  border-radius: 12px;
+`;
+
+export const LiveGameOver = styled.View`
+  position: absolute;
+  flex-direction: row;
+  align-items: center;
+  top: 8px;
+  right: 16px;
+`;
+
+export const LiveGameTitle = styled.View`
+  background-color: #5f80ff;
+  border-radius: 4px;
+  padding: 2px 8px;
+  margin-right: 4px;
+`;
+
+export const LiveGameBadge = styled.View`
+  flex-direction: row;
+  background-color: #ff1b4a;
+  border-radius: 4px;
+  padding: 2px 8px;
+`;
